553ed6abbbcec1186de3b3360a0d8034cd8ea2e6
Travis Cline <travis.cline@gmail.com>
2026-04-02 16:31:30 -0700

    pkg/axuiautomation: remove deprecated compatibility shim
    All consumers already import github.com/tmc/apple/x/axuiautomation directly.
    The shim re-exported that package but had zero importers.

--- pkg/axuiautomation/doc.go
+++
@@ -1,5 +0,0 @@
-// Package axuiautomation is a compatibility shim.
-//
-// Deprecated: use github.com/tmc/apple/x/axuiautomation directly.
-// This package re-exports that implementation to preserve existing imports.
-package axuiautomation
